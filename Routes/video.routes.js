9d3ec93f862b4b11c6fe305795e8c639ee075c05
Mukul Saini <77503907+imukulsaini@users.noreply.github.com>
2021-12-29 00:28:26 +0530

    update video route

--- Routes/video.routes.js
+++ Routes/video.routes.js
@@ -1,84 +1,110 @@
 const express = require("express");
 const router = express.Router();
 const { extend } = require("lodash");
+const { checkVideoID } = require("../controllers/videos.controller");
 
 const { Video } = require("../Modals/video.modal");
+const { Category } = require("../Modals/category.modal");
+
+
+
+
+
 
 router
-  .route("/")
-  .get(async (req, res) => {
-    try {
-      const videos = await Video.find({});
+    .route("/")
 
-      res.json({
-        status: 201,
-        message: "videos Fetching Successfully Done ",
-        videos,
-      });
-    } catch (error) {
-      res.json({
-        status: 401,
-        message: error,
-      });
-    }
-  })
+    .get(async (req, res) => {
+        const videos = await Video.find({});
 
-  .post(async (req, res) => {
-    try {
-      const video = req.body;
+        res
+            .status(200)
+            .json({ message: "videos Fetching Successfully Done ", videos });
+    })
 
-      const NewVideo = new Video(video);
-      const savedVideo = await NewVideo.save();
-      res.json({
-        status: 400,
-        message: "new video saved Successfully ",
-        video: savedVideo,
-      });
-    } catch (error) {
-      res.json({
-        status: 500,
-        message: "video is not saved",
-        errorMessage: error.message,
-      });
-    }
-  });
+    .post(async (req, res) => {
+        try {
+            const video = req.body;
 
-router.param("videoId", async (req, res, next, id) => {
-  try {
-    const video = await Video.findById(id);
+            const NewVideo = new Video(video);
+            const savedVideo = await NewVideo.save();
+            res.status(201).json({
+                message: "new video saved Successfully ",
+                video: savedVideo,
+            });
+        } catch (error) {
+            res.status(400).json({
+                message: error.message,
+            });
+        }
+    });
 
-    if (!video) {
-      res.json({ status: 401, message: "Video id is not valid " });
-    } else {
-      req.video = video;
 
-      return next();
-    }
-  } catch (error) {
-    res.json({ status: 410, message: error.message });
-  }
-});
+router.route('/trending')
+    .get(async (req, res) => {
+        const videos = await Video.find({})
+            .sort({ 'views': -1 })
+            ;
+
+        res
+            .status(200)
+            .json({ message: "videos Fetching Successfully Done ", trendingVideos: videos });
+    })
+
+
+router.route("/category/:categoryName")
+
+    .get(async (req, res) => {
+        const { categoryName } = req.params;
+
+
+        const isCategoryExist = await Category.find({ name: categoryName });
+        if (isCategoryExist) {
+
+            const getCategoriesVideos = await Video.find({ category: categoryName });
+
+            res.status(200).json({
+                message: "Categories Videos Fetch Successful",
+                videos: getCategoriesVideos,
+            });
+
+        }
+        else {
+            res.status(404).json({ message: " category name is not found " })
+        }
+
+    });
+
+
+
+
+router.param("videoID", checkVideoID);
 
 router
-  .route("/:videoId")
-  .get((req, res) => {
-    let { video } = req;
-    video.__v = undefined;
+    .route("/:videoID")
 
-    res.json({ status: 201, video });
-  })
-  .post(async (req, res) => {
-    let { video } = req;
-    const updateVideo = req.body;
-    video = extend(video, updateVideo);
-    video = await video.save();
-    res.json({ status: 201, message: "video Updated", video });
-  })
+    .get((req, res) => {
+        let { video } = req;
+        video.__v = undefined;
 
-  .delete(async (req, res) => {
-    let { video } = req;
-    video = await video.remove();
-    res.json({ status: 201, message: "video deleted" });
-  });
+        res.status(200).json({ video });
+    })
+
+    .post(async (req, res) => {
+        let { video } = req;
+        const updateVideo = req.body;
+        video = extend(video, updateVideo);
+        video = await video.save();
+        res.status(201).json({ message: "video Updated", video });
+    })
+
+    .delete(async (req, res) => {
+        let { video } = req;
+        video = await video.remove();
+        res.status(204);
+    });
+
+
+
 
 module.exports = router;
