161f372e354dbd1011f5f1fd426ba5f51415090f
Mukul Saini <77503907+imukulsaini@users.noreply.github.com>
2021-12-29 00:27:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,76 +1,68 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+const cors = require('cors')
 const app = express();
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
-app.use(cors());
+const jwt = require('jsonwebtoken');
 
-const mySecret = process.env["keySecret"];
+// const authVerify = require('./middlewares/authverify.middleware.js')
 
-function authVerify(req, res, next) {
-  const token = req.headers.authorization;
+const authVerify = require('./middlewares/authVerify.middleware.js')
+const { routeErrorHandler } = require('./middlewares/routeErrorHandler.middleware.js');
+const { errorHandler } = require('./middlewares/errorHandler.middleware.js');
+const mySecret = process.env['keySecret']
 
-  try {
-    let decoded = jwt.verify(token, mySecret);
-    req.user = { userID: decoded.userID };
-    return next();
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ message: "Unauthorized access" });
-  }
-}
 
+app.use(cors())
 app.use(bodyParser.json());
 
-//db connect
 
-const { initializeDBConnection } = require("./db/db.connect.js");
+
+// db connect 
+
+const { initializeDBConnection } = require("./db/db.connect.js")
 const PORT = 3000;
 initializeDBConnection();
 
-const videoV1 = require("./Routes/video.routes");
-const userV1 = require("./Routes/user.routes");
+
+
+
+const videoV1 = require("./Routes/video.routes")
+const userV1 = require("./Routes/user.routes")
 const likedVideoV1 = require("./Routes/likedvideos.routes.js");
-const watchLaterV1 = require("./Routes/watchlater.routes.js");
+const watchLaterV1 = require("./Routes/watchlater.routes.js")
 const playListV1 = require("./Routes/playlist.routes.js");
 
-app.use("/videos", videoV1);
-app.use("/", userV1);
 
-//private Route '/me'
+
+
+
+
+app.use("/v1/videos", videoV1)
+app.use("/v1/", userV1)
+
+// private Route 
 
 app.use("/v1/", authVerify, likedVideoV1);
-app.use("/v1/", authVerify, watchLaterV1);
-app.use("/v1/", authVerify, playListV1);
+app.use("/v1/", authVerify, watchLaterV1)
+app.use("/v1/", authVerify, playListV1)
+
+
 
 // 404 Route Handler
 
-app.use((req, res) => {
-  res
-    .status(404)
-    .json({
-      success: false,
-      message: "route not found on server, please check",
-    });
-});
+app.use(routeErrorHandler)
+
+
 
 //  Error Handler
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res
-    .status(500)
-    .json({
-      success: false,
-      message: "error occurred, see the errMessage key for more details",
-      errorMessage: err.message,
-    });
-});
+app.use(errorHandler)
+
 
 // port establish
 
+
 app.listen(3000, () => {
-  console.log("server started");
+    console.log('server started');
 });
